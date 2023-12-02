85f3864a327d5b9db0cf11128f098e508767333e
mungvv <mung.vu@bnksolution.com>
2023-12-02 16:46:24 +0700

    #3-create-reponsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ function App() {
       <h1 className="text-primary test-p p-2 m-3 bg-hover-error-light-3 o-5  ">
         hello
       </h1>
-      <div className="display-flex" style={{ width: "100%",}}>
+      <div
+        className="display-flex bg-black"
+        style={{ width: "100%", height: "100px" }}
+      >
         <div
-          className="bg-orange"
+          className=" color-test"
+          style={{ height: "50px", width: "30%" }}
+        ></div>
+        <div
+          className="bg-blue br"
           style={{ width: "30%", height: "50px" }}
         ></div>
-        <div className="bg-blue br" style={{ width: "30%", height: "50px" }}></div>
         <div className="bg-red" style={{ width: "30%", height: "50px" }}></div>
       </div>
     </>
